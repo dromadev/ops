70cf387507642630cd7ffca87151718c95d7fae3
dromadev <ma.chaibi@gmail.com>
2019-01-16 16:55:19 +0100

    Update start.sh

--- bastion/start.sh
+++ bastion/start.sh
@@ -14,4 +14,6 @@ ssh-keygen -t rsa -b 4096 -C "$(whoami)@$(hostname)-$(date)" -f ./id_rsa -q -N "
 echo "Copie de la cle  ssh key sur chaque conteneur -- password est  dromadev " 
 docker ps --format "{{.Names}}" | grep "centos" | xargs -i docker inspect -f "{{ .NetworkSettings.IPAddress }}" {} | xargs -i ssh-copy-id -i ./id_rsa root@{}
 echo "Creation du fichier inventaire Ansible " 
-echo [centos] > hosts
+echo [cobayes] > hosts
+docker ps --format "{{.Names}}" | grep "cobaye" | xargs -i docker inspect -f "{{ .NetworkSettings.IPAddress }}" {} | xargs -i echo {} ansible_user=root >> hosts
+docker run -it --rm -v `pwd`/hosts:/etc/ansible/hosts -v `pwd`/id_rsa:/root/.ssh/id_rsa chaibim/centos-bastion all -m ping
